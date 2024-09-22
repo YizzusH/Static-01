51d2826647e92717cd23fc9dc11a0d10ff30ab1d
Yizzu <agroinversionessas01@gmai.com>
2024-09-21 22:17:10 -0500

    graficar variables

--- Cargue de datos.R
+++ Cargue de datos.R
@@ -13,3 +13,26 @@ datos <- read_excel(ruta_archivo)
 # Mostrar los primeros registros del archivo head para los primeros 6 ,
 # Mostrar todos los registros Print
 head(datos)
+
+# visualizacion
+
+install.packages("ggplot2")
+install.packages("gridExtra")
+library(ggplot2)
+library(gridExtra)
+
+# Crear histogramas
+hist_modificado <-ggplot(datos, aes(x = Modificado)) +
+  geom_histogram(binwidth = 0.2, fill = "blue", color = "white") +
+  ggtitle("Histograma de Modificado") +
+  xlab("Valores") +
+  ylab("Frecuencia")
+
+hist_sin_modificar <-ggplot(datos, aes(x = `Sin Modificar`)) +
+  geom_histogram(binwidth = 0.2, fill = "red", color = "white") +
+  ggtitle("Histograma de Sin Modificar") +
+  xlab("Valores") +
+  ylab("Frecuencia")
+
+# Mostrar ambos gráficos en una sola visualización
+grid.arrange(hist_modificado, hist_sin_modificar, ncol = 2)
